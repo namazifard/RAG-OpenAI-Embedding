541e086687ada4b76ddfa3dded622a5c0dd14b8f
Danial Namazifard <107290809+namazifard@users.noreply.github.com>
2024-07-16 01:59:41 +0330

    Add files via upload

---
+++ query_data.py
@@ -0,0 +1,70 @@
+import argparse
+import os
+from langchain_community.vectorstores import Chroma
+# from langchain.vectorstores.chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+# from langchain_community.llms.ollama import Ollama
+
+from get_embedding_function import get_embedding_function
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import openai
+
+
+CHROMA_PATH = "chroma"
+
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
+
+
+def query_rag(query_text: str):
+    # Prepare the DB.
+    embedding_function = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+    return response_text
+
+
+if __name__ == "__main__":
+    main()
